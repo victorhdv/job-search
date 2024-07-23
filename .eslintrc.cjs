f45e986c14606e4617ba2e189895a7f0e720b819
Victor Hugo Duarte do Vale <victorhugoddv@gmail.com>
2024-07-23 17:04:09 -0300

    configurando os testes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,8 +3,16 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  extends: ['plugin:vue/vue3-recommended', 'eslint:recommended', '@vue/eslint-config-prettier'],
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier',
+    'plugin:vitest-globals/recommended'
+  ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  env: {
+    'vitest-globals/env': true
   }
 }
