38c44ce737bb6d21016f8792aaa29ef36ee847bc
Victor Hugo Duarte do Vale <victorhugoddv@gmail.com>
2024-07-24 17:51:12 -0300

    criado o nav

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,12 +2,29 @@
   <header class="w-full text-sm">
     <div class="fixed left-0 top-0 h-16 w-full bg-white">
       <div class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8">
-        <a
-          href="https://github.com/victorhdv"
-          target="_blank"
-          class="flex h-full items-center text-xl"
-          >{{ company }}</a
-        >
+        <a v-bind:href="url" target="_blank" class="flex h-full items-center text-xl font-bold">{{
+          company
+        }}</a>
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full list-none">
+            <li class="h-full"><a href="/" class="flex h-full items-center py-2.5">Teams</a></li>
+            <li class="ml-9 h-full">
+              <a href="/" class="flex h-full items-center py-2.5">Location</a>
+            </li>
+            <li class="ml-9 h-full">
+              <a href="/" class="flex h-full items-center py-2.5">Life at Vale Corp</a>
+            </li>
+            <li class="ml-9 h-full">
+              <a href="/" class="flex h-full items-center py-2.5">How we hire</a>
+            </li>
+            <li class="ml-9 h-full">
+              <a href="/" class="flex h-full items-center py-2.5">Students</a>
+            </li>
+            <li class="ml-9 h-full">
+              <a href="/" class="flex h-full items-center py-2.5">Jobs</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -18,7 +35,8 @@ export default {
   name: 'MainNav',
   data() {
     return {
-      company: 'Vale Careers'
+      company: 'Vale Careers',
+      url: 'https://github.com/victorhdv'
     }
   }
 }
